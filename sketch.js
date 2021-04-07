df2ecfadacdea6c540e041ce0cf55c2238499f80
BhavyaShrivastava <79461483+BhavyaShrivastava@users.noreply.github.com>
2021-04-07 12:09:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -45,13 +45,13 @@ function keyPressed(){
 
   //For moving and changing animation write code here
 
-  if(keycode === LEFT_ARROW){
+  if(keycode == LEFT_ARROW){
       Mouse.addAnimation("mouseTeasing",mouseimg2);
       mouse.changeAnimation("mouseTeasing");
       mouse.frameDelay = 25;
   }
 
-   if(keycode === RIGHT_ARROW){
+   if(keycode == RIGHT_ARROW){
      cat.addAnimation("catRunning",catimg2);
      cat.changeAnimation("catRunning");
    }
