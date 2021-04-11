2db0b944e79e334fd3344d212f31325fe27ffd87
BhavyaShrivastava <79461483+BhavyaShrivastava@users.noreply.github.com>
2021-04-11 20:08:47 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -21,7 +21,7 @@ function setup(){
   mouse.addAnimation( "mouseimage" ,mouseimg1);
   mouse.scale = 0.2;
 
-  cat = createSprite(500,600,10,10);
+  cat = createSprite(800,600,10,10);
   cat.addAnimation( "catimage" ,catimg1);
   cat.scale = 0.2;
 
@@ -35,6 +35,7 @@ function draw() {
 if(cat.x - mouse.x < (cat.width - mouse.width)/2){
   cat.addAnimation("catsite",catimg3);
   mouse.addAnimation("mousehappy",mouseimg3);
+  cat.velocityX = 0;  
 }
 
 
